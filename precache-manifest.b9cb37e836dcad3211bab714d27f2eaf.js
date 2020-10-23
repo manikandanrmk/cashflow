bf5fbf6e5687e676f4f36447878b1cd7f0f34d29
Manikandan <manikandanrmk222@gmail.com>
2020-10-23 21:59:31 +0530

    Updates

---
+++ precache-manifest.b9cb37e836dcad3211bab714d27f2eaf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "461d2aaf58984f059c0c5470799a3ab4",
+    "url": "/cashflow/index.html"
+  },
+  {
+    "revision": "812afb84cf5e4696216b",
+    "url": "/cashflow/static/css/main.cbb14dbf.chunk.css"
+  },
+  {
+    "revision": "0721b6c8c41ee3a4d687",
+    "url": "/cashflow/static/js/2.c15d1e97.chunk.js"
+  },
+  {
+    "revision": "376a73e96f34daa93344ea884b45fa5d",
+    "url": "/cashflow/static/js/2.c15d1e97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "812afb84cf5e4696216b",
+    "url": "/cashflow/static/js/main.c953c10d.chunk.js"
+  },
+  {
+    "revision": "8e110acb9ae892ce4d18",
+    "url": "/cashflow/static/js/runtime-main.65bcebeb.js"
+  }
+]);
